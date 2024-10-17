b3472b5644e9a52678efeb204868fe3d32426d63
sivakumar0703 <ssivakumar358@gmail.com>
2024-10-17 18:40:15 +0530

    added render url

--- src/GlobalState/GamingContext.js
+++ src/GlobalState/GamingContext.js
@@ -9,8 +9,8 @@ let socket;
 let isMounted;
 
 const GamingContext = ({ children }) => {
-  const baseUrl = "http://localhost:8000";
-  const url = "http://localhost:8000/api/rps";
+  const baseUrl = "https://rock-paper-scissor-game-be.onrender.com";
+  const url = "https://rock-paper-scissor-game-be.onrender.com/api/rps";
   const [createButton , setCreateButton] = useState("CREATE");
   const [joinButton , setJoinButton] = useState("JOIN");
   const [showGameArea, setShowGameArea] = useState(false);
